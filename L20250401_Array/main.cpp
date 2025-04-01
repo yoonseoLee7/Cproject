51fbe1ed06712c6352c5b6beabf38a48b26139a5
zzzzS2 <148181743+yoonseoLee7@users.noreply.github.com>
2025-04-01 11:47:33 +0900

    디버깅하는 방법 추가 설명

--- L20250401_Array/main.cpp
+++ L20250401_Array/main.cpp
@@ -15,16 +15,30 @@ int main()
 	//cout << Average << endl;
 
 	//반복문 과 배열
-	int Korean[10];
+	//int Korean[10];
+
+
 
 	//printf("%x\n", Korean);
 	//printf("%x\n", &Korean[0]);
 	//printf("%x\n", &Korean[1]);
 
-	for (int i = 0; i < 10; i++)
-	{
-		Korean[i] = 3;
-	}
+	//for (int i = 0; i < 2; i++)
+	//{
+	//	Korean[i] = 3;
+	//}
+
+	// F10 한줄씩 디버깅
+	// F9 한줄 멈춤
+	// shift+F5 디버깅 종료
+
+	//Goblins가 한줄 디버깅할 대 Autos에서 안보이는 이유 = 초기화가 안되어있기때문에 Value 값이 너무 큼(값이 너무 크면 버림)
+
+	int A = 0;
+
+	int Goblins[5];
+	float MP[3];
+	char Gold[7];
 
 	return 0;
 }
