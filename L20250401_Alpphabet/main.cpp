ac3e0f3ad3d087ac891ca496d9a57a44c1c049d1
zzzzS2 <148181743+yoonseoLee7@users.noreply.github.com>
2025-04-01 16:58:25 +0900

    제어,반복문 복습 commit

--- L20250401_Alpphabet/main.cpp
+++ L20250401_Alpphabet/main.cpp
@@ -4,13 +4,32 @@ using namespace std;
 
 int main()
 {
-
+	//초기화
 	float Color = 0;
 	int Apple = 0;
 	int Goblin = 0;
 	char Alphabet = 'A';
 	bool Condition = true;
 	bool IsFinish = false;
+	bool IsRunning = true;
+
+	char Key = 0;
+
+	while (IsRunning)
+	{
+		//Input
+		cin >> Key;
+
+		//Process
+		if (Key == 'q')
+		{
+			IsRunning = false;
+		}
+
+		//Render
+		cout << "모이셈" << endl;
+	}
+
 
 	return 0;
 }
