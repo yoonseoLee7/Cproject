dc3eb34850fb2b0c2cc5ba39e2fe2f3f38a2ae8d
zzzzS2 <148181743+yoonseoLee7@users.noreply.github.com>
2025-04-07 09:45:22 +0900

    중복제거

--- L20250407_test01/main.cpp
+++ L20250407_test01/main.cpp
@@ -9,11 +9,29 @@ int main()
 	srand(time(NULL));
 	int ball[52];
 
+	//초기화 공 1~52
 	for (int i = 0; i < 52; i++)
 	{
-		ball[i] = rand() % 53;
+		ball[i] = i + 1;
+	}
+
+	//중복제거
+	for (int i = 51; i > 0; i--)
+	{
+		int j = rand() % (i + 1); 
+		swap(ball[i], ball[j]);
+	}
+
+
+	for (int i = 0; i < 52; i++)
+	{
 		cout << ball[i] << endl;
 	}
 
+
+	/*ball[i] = rand() % 53;
+	cout << ball[i] << endl;*/
+
 	return 0;
 }
+
