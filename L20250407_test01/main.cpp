ff6bfb5bd70fbecf4fb9503f18dab6ffa0684c78
zzzzS2 <148181743+yoonseoLee7@users.noreply.github.com>
2025-04-07 11:30:23 +0900

    수정_함수화

--- L20250407_test01/main.cpp
+++ L20250407_test01/main.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
 #include <stdlib.h>
-#include <time.h>
+
+#define BallSize	52
 
 using namespace std;
 
-int main()
-{
-	srand(time(NULL));
-	int ball[52];
 
+int ball[BallSize];
+
+void Initialize()
+{
 	//초기화 공 1~52
-	for (int i = 0; i < 52; i++)
+	for (int i = 0; i < BallSize; i++)
 	{
 		ball[i] = i + 1;
 	}
 
+}
+
+void Shuffle()
+{
 	//중복제거
-	for (int i = 51; i > 0; i--)
+	for (int i = BallSize - 1; i > 0; i--)
 	{
-		int j = rand() % (i + 1); 
+		int j = rand() % (i + 1);
 		swap(ball[i], ball[j]);
 	}
+}
 
-
-	for (int i = 0; i < 52; i++)
+void PrintBall()
+{
+	for (int i = 0; i < BallSize; i++)
 	{
 		cout << ball[i] << endl;
 	}
+}
+
+int main()
+{
+	srand(time(NULL));
 
 
-	/*ball[i] = rand() % 53;
-	cout << ball[i] << endl;*/
+	Initialize();
+	Shuffle();
+	PrintBall();
 
 	return 0;
 }
