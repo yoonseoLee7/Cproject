ee3c5c26f2cb465f86bf4b8d9cd963c9f9063864
zzzzS2 <148181743+yoonseoLee7@users.noreply.github.com>
2025-04-21 09:52:21 +0900

    탬플릿으로 사칙연산

---
+++ L20250421_Polymophism/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+//STL -> standard template library
+//탬플릿은 에러가 나면 코드를 추적하기가 어려움
+using namespace std;
+
+template <typename T> //자바의 제네릭이랑 비슷
+class T_FourBasic
+{
+public:
+	T Add(T A, T B);
+	T Subtract(T A, T B);
+	T Multiply(T A, T B);
+	T Divide(T A, T B);
+};
+
+int main()
+{
+	T_FourBasic<int> A;
+
+	cout << A.Add(10, 2) << endl;
+	cout << A.Subtract(10,2) << endl;
+	cout << A.Multiply(10,2) << endl;
+	cout << A.Divide(10,2) << endl;
+		
+	return 0;
+}
+
+template<typename T>
+T T_FourBasic<T>::Add(T A, T B)
+{
+	return A + B;
+}
+
+template<typename T>
+T T_FourBasic<T>::Subtract(T A, T B)
+{
+	return A - B;
+}
+
+template<typename T>
+T T_FourBasic<T>::Multiply(T A, T B)
+{
+	return A * B;
+}
+
+template<typename T>
+T T_FourBasic<T>::Divide(T A, T B)
+{
+	return A / B;
+}
